b26458621aa8776b4054b881593bbc7508d7c217
Xingyou Song <xingyousong@google.com>
2022-04-22 10:51:53 -0700

    Internal change
    PiperOrigin-RevId: 443706519

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 sudo apt-get install -y libprotobuf-dev  # Needed for proto libraries.
 
 # Installs Python packages.
-pip install -r requirements.txt # Installs dependencies
+pip install -r requirements.txt --use-deprecated=legacy-resolver # Installs dependencies
 pip install -e . # Installs Vizier
 
 # Builds all .proto files.
