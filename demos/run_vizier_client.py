9ca8b5a9c7af2b451e164f8707c711b393b5af21
Xingyou Song <xingyousong@google.com>
2022-07-08 10:54:35 -0700

    Change demo to use RANDOM_SEARCH for now due to https://github.com/google/vizier/issues
    PiperOrigin-RevId: 459791307

--- demos/run_vizier_client.py
+++ demos/run_vizier_client.py
@@ -81,7 +81,7 @@ def main(argv: Sequence[str]) -> None:
             name='latency', goal=vz.ObjectiveMetricGoal.MINIMIZE))
 
   if FLAGS.multiobjective:
-    study_config.algorithm = vz.Algorithm.NSGA2
+    study_config.algorithm = vz.Algorithm.RANDOM_SEARCH
   else:
     study_config.algorithm = vz.Algorithm.EMUKIT_GP_EI
 
