c780c4c71a51a3d473f802bdd91ed031c68a56c3
Xingyou Song <xingyousong@google.com>
2022-08-26 07:44:07 -0700

    Adding Nasbench-101 experimenter + adding infeasibility. Also reorganize CI dependencies.
    PiperOrigin-RevId: 470243819

--- vizier/benchmarks/nas/__init__.py
+++ vizier/benchmarks/nas/__init__.py
@@ -1,2 +1,3 @@
 """Neural Architecture Search (heavyweight) benchmark classes for Vizier."""
+from vizier._src.benchmarks.experimenters.nasbench101_experimenter import NASBench101Experimenter
 from vizier._src.benchmarks.experimenters.nasbench201_experimenter import NASBench201Experimenter
