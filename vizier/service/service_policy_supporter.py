06146598fd7857a64a9399d0aaf7b2147b122b97
Xingyou Song <xingyousong@google.com>
2022-09-20 07:30:01 -0700

    Use TrialFilter in ServicePolicySupporter and follow exact PolicySupporter base class API.
    PiperOrigin-RevId: 475549890

--- vizier/service/service_policy_supporter.py
+++ vizier/service/service_policy_supporter.py
@@ -31,15 +31,12 @@ class ServicePolicySupporter(pythia.PolicySupporter):
     self._study_guid = study_guid
     self._vizier_service = vizier_service
 
-  def GetStudyConfig(self,
-                     study_guid: Optional[str] = None) -> vz.ProblemStatement:
-    if study_guid is None:
-      study_guid = self._study_guid
+  def GetStudyConfig(self, study_guid: str) -> vz.ProblemStatement:
     request = vizier_service_pb2.GetStudyRequest(name=study_guid)
     study = self._vizier_service.GetStudy(request)
     return pyvizier.StudyConfig.from_proto(study.study_spec).to_problem()
 
-  # TODO: Use TrialsFilter instead.
+  # TODO: Support filters in ListTrialsRequest.
   def GetTrials(
       self,
       *,
@@ -49,7 +46,7 @@ class ServicePolicySupporter(pythia.PolicySupporter):
       max_trial_id: Optional[int] = None,
       status_matches: Optional[vz.TrialStatus] = None,
       include_intermediate_measurements: bool = True) -> List[vz.Trial]:
-    """Requests Trials from Vizier."""
+    """Fetch all trials and then apply the filter."""
 
     if study_guid is None:
       study_guid = self._study_guid
@@ -57,27 +54,12 @@ class ServicePolicySupporter(pythia.PolicySupporter):
     trials = self._vizier_service.ListTrials(request).trials
     all_pytrials = pyvizier.TrialConverter.from_protos(trials)
 
-    if trial_ids is not None:
-      # We're going to do repeated comparisons in _trial_filter(), so this
-      # needs to be more real than an Iterator.
-      trial_ids = frozenset(trial_ids)
-
-    def _trial_filter(trial) -> bool:
-      if trial_ids is not None and trial.id not in trial_ids:
-        return False
-      else:
-        if min_trial_id is not None:
-          if trial.id < min_trial_id:
-            return False
-        if max_trial_id is not None:
-          if trial.id > max_trial_id:
-            return False
-        if status_matches:
-          if trial.status != status_matches:
-            return False
-      return True
-
-    filtered_pytrials = [t for t in all_pytrials if _trial_filter(t)]
+    trial_filter = vz.TrialFilter(
+        ids=trial_ids,
+        min_id=min_trial_id,
+        max_id=max_trial_id,
+        status=[status_matches] if status_matches else None)
+    filtered_pytrials = [t for t in all_pytrials if trial_filter(t)]
 
     # Doesn't affect datastore when measurements are deleted.
     if not include_intermediate_measurements:
