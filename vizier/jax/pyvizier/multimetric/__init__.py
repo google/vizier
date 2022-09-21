771dfba497b619eb8a7b5cf76424a14386f0d600
Chansoo Lee <chansoo@google.com>
2022-09-21 09:06:37 -0700

    Move jax-enabled multimetric utilities to `pyvizier/multimetric/xla_pareto.py`
    PiperOrigin-RevId: 475843777

--- vizier/jax/pyvizier/multimetric/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Init module."""
-
-from vizier._src.jax import xla_pareto
-from vizier._src.jax.xla_pareto import JaxParetoOptimalAlgorithm
---
+++ vizier/pyvizier/multimetric/xla_pareto.py
@@ -0,0 +1,4 @@
+"""XLA-enabled algorithm for determining pareto optimality."""
+# pylint: disable=unused-import
+
+from vizier._src.jax.xla_pareto import JaxParetoOptimalAlgorithm
