dd103f46212d63d4fab402cb8b7bf6b8efde7cb5
Richard Zhang <qiuyiz@google.com>
2022-09-27 07:55:38 -0700

    Add convergence curve converters (from Trial) and curve analysis tools.
    PiperOrigin-RevId: 477171017

---
+++ vizier/_src/benchmarks/analyzers/convergence_curve.py
@@ -0,0 +1,279 @@
+"""Converters and comparators for convergence curves."""
+
+import dataclasses
+import enum
+from typing import Callable, List, Optional, Sequence, Union
+
+import numpy as np
+from vizier import pyvizier
+
+
+@dataclasses.dataclass
+class ConvergenceCurve:
+  """Represents a batch of convergence curves on the same task."""
+  xs: np.ndarray  # [T] array. All curves share the x axis.
+  ys: np.ndarray  # [N x T] array where N is the batch size.
+  ylabel: str = ''  # Optional for plotting.
+  xlabel: str = ''  # Optional for plotting.
+  # Indicates ys should be increasing in t or decreasing.
+
+  @enum.unique
+  class YTrend(enum.Enum):
+    """Trend of ys across t."""
+    UNKNOWN = 'unknown'
+    INCREASING = 'increasing'
+    DECREASING = 'decreasing'
+
+  trend: YTrend = YTrend.UNKNOWN
+
+  @property
+  def batch_size(self) -> int:
+    return self.ys.shape[0]
+
+  @classmethod
+  def align_xs(cls,
+               curves: Sequence['ConvergenceCurve'],
+               resolution: Optional[int] = None) -> 'ConvergenceCurve':
+    """Align curves to the same xs using linear interpolation.
+
+    Args:
+      curves:
+      resolution: Number of points to interpolate to. Leave it None to use the
+        maximal resolution.
+
+    Returns:
+      ConvergenceCurve whose batch size is equal to the sum of the batch size
+      of `curves`.
+    """
+    if not curves:
+      raise ValueError('Empty sequence of curves.')
+    if len(set([c.trend for c in curves])) > 1:
+      raise ValueError('All curves must be increasing or decreasing.')
+    minx = np.min([np.min(c.xs) for c in curves])
+    maxx = np.max([np.max(c.xs) for c in curves])
+    resolution = resolution or np.max([np.size(c.xs) for c in curves])
+    xs = np.linspace(minx, maxx, resolution)
+
+    all_ys = []
+    for curve in curves:
+      for ys in curve.ys:
+        all_ys.append(np.interp(xs, curve.xs, ys, right=np.nan))
+
+    # Take all non-empty ylabels.
+    ylabels = list(set([c.ylabel for c in curves if c.ylabel]))
+    if not ylabels:
+      ylabel = ''
+    elif len(ylabels) > 1:
+      print('Curves have different ylabels: %s.', ylabels)
+      ylabel = ''
+    else:
+      ylabel = ylabels[0]
+
+    return cls(xs=xs, ys=np.stack(all_ys), ylabel=ylabel, trend=curves[0].trend)
+
+
+class ConvergenceCurveConverter:
+  """Converter for Trial sequence to ConvergenceCurve."""
+
+  def __init__(self,
+               metric_information: pyvizier.MetricInformation,
+               *,
+               flip_signs: bool = False,
+               cost_fn: Callable[[pyvizier.Trial], Union[float,
+                                                         int]] = lambda _: 1,
+               measurements_type: str = 'final'):
+    """Init.
+
+    Args:
+      metric_information: Information of relevant metric.
+      flip_signs: If True, flips the signs of metric values to always
+      maximize. Useful when desiring all increasing curves.
+      cost_fn: Cost of each Trial (to determine xs in ConvergenceCurve).
+      measurements_type: ['final', 'intermediate', 'all']
+    """
+    self.metric_information = metric_information
+    self.flip_signs = flip_signs
+    self.cost_fn = cost_fn
+    self.measurements_type = measurements_type
+
+  def convert(self, trials: Sequence[pyvizier.Trial]) -> ConvergenceCurve:
+    """Returns ConvergenceCurve of batch size 1."""
+    yvals = [np.nan]
+    xvals = [0]
+    comparator = self.comparator
+    for trial in trials:
+      candidates = [np.nan]
+      if self.measurements_type in ('final', 'all'):
+        if trial.final_measurement and (self.metric_information.name
+                                        in trial.final_measurement.metrics):
+          candidates.append(trial.final_measurement.metrics[
+              self.metric_information.name].value)
+      if self.measurements_type in ('intermediate', 'all'):
+        for measurement in trial.measurements:
+          if self.metric_information.name in measurement.metrics:
+            candidates.append(
+                measurement.metrics[self.metric_information.name].value)
+
+      yvalue = comparator(candidates)
+      xvals.append(xvals[-1] + self.cost_fn(trial))
+      yvals.append(comparator([yvalue, yvals[-1]]))
+
+    yvals = np.asarray(yvals[1:])
+    trend = ConvergenceCurve.YTrend.DECREASING
+    if (self.metric_information.goal == pyvizier.ObjectiveMetricGoal.MAXIMIZE
+       ) or (self.metric_information.goal
+             == pyvizier.ObjectiveMetricGoal.MINIMIZE and self.flip_signs):
+      trend = ConvergenceCurve.YTrend.INCREASING
+    return ConvergenceCurve(
+        xs=np.asarray(xvals[1:]),
+        ys=np.asarray(yvals).reshape([1, -1]) * (-1 if self.flip_signs else 1),
+        trend=trend,
+        ylabel=self.metric_information.name)
+
+  @property
+  def comparator(self):
+    """Comparator used for creating the convergence curve."""
+    return np.nanmax if (
+        self.metric_information.goal
+        == pyvizier.ObjectiveMetricGoal.MAXIMIZE) else np.nanmin
+
+
+class ConvergenceCurveComparator:
+  """Comparator methods for ConvergenceCurves.
+
+  Methods in this class generally return comparison metrics for a compared curve
+  against a baseline curve. Only works for curves with INCREASING or DECREASING
+  trend.
+
+  Example usage:
+    baseline_curve = ConvergenceCurve(...)
+    comparator = ConvergenceCurveComparator(baseline_curve)
+    comparator.log_efficiency_curve(compared_curve)
+  """
+
+  def __init__(self, baseline_curve: ConvergenceCurve):
+    """Initialize class with baseline curve.
+
+    Args:
+      baseline_curve: A baseline ConvergenceCurve to compare against.
+
+    Raises:
+      ValueError: If baseline curve is not INCREASING or DECREASING.
+    """
+    if baseline_curve.trend not in (ConvergenceCurve.YTrend.INCREASING,
+                                    ConvergenceCurve.YTrend.DECREASING):
+      raise ValueError(f'Curve trend {baseline_curve.trend} must be either'
+                       'increasing or decreasing.')
+    self._baseline_curve = baseline_curve
+    self._sign = 1.0 if (self._baseline_curve.trend
+                         == ConvergenceCurve.YTrend.INCREASING) else -1.0
+
+  def log_efficiency_curve(self,
+                           compared_curve: ConvergenceCurve,
+                           baseline_quantile: float = 0.5,
+                           compared_quantile: float = 0.5) -> ConvergenceCurve:
+    """Builds the log sample efficiency curve.
+
+    The compared curve should approximately use exp(-relative efficiency)% less
+    Trials than the baseline curve. Note that a positive relative effiency
+    demonstrates that the compared curve is better than the baseline. Also,
+    the relative efficiency CURVES are not fully symmetric due to differences
+    in drops in objective values.
+
+    Args:
+      compared_curve: Compared convergence curve.
+      baseline_quantile: Quantile in [0, 1] of the batched baseline curve to use
+        for efficiency comparison. The higher the quantile, the better the
+        quality of the baseline batch.
+      compared_quantile: Quantile in [0, 1] of the batched compared curve to use
+        for efficiency comparison. The higher the quantile, the better the
+        quality of the baseline batch.
+
+    Returns:
+      ConvergenceCurves with ys (batch size 1) as the relative efficiency curve.
+
+      The xs of curve is equal to the xs of baseline curve but there
+      may be indices equal to positive 'inf' which indicate that no point
+      in the compared curve outperforms the baseline at that index.
+
+    Raises:
+      ValueError: If the trends do mismatch.
+      ValueError: If baseline_quantile or compared_quantile are not in [0, 1].
+    """
+    if self._baseline_curve.trend != compared_curve.trend:
+      raise ValueError(
+          f'Baseline curve trend {self._baseline_curve.trend}'
+          f' must match compared curve trend {compared_curve.trend}')
+    baseline_quantile = np.nanquantile(
+        self._sign * self._baseline_curve.ys, baseline_quantile, axis=0)
+    # This may not be [1,2,...] due to repeats.
+    baseline_index_curve = build_convergence_curve(baseline_quantile,
+                                                   baseline_quantile)
+
+    other_index_curve = build_convergence_curve(
+        baseline_quantile,
+        np.nanquantile(
+            self._sign * compared_curve.ys, compared_quantile, axis=0))
+
+    ys = np.log(1 + np.asarray(baseline_index_curve)) - np.log(
+        1 + np.asarray(other_index_curve))
+    return ConvergenceCurve(
+        xs=self._baseline_curve.xs, ys=ys.reshape(1, len(ys)))
+
+  def get_log_efficiency_score(self,
+                               compared_curve: ConvergenceCurve,
+                               baseline_quantile=0.5,
+                               compared_quantile=0.5) -> float:
+    """Gets a finalized log efficiency score.
+
+    The compared curve should approximately use exp(-score)% Trials compared to
+    the baseline curve. Note that a high positive score demonstrates that the
+    compared curve uses less Trials and is better than the baseline.
+
+    Args:
+      compared_curve: Compared convergence curve.
+      baseline_quantile: Quantile in [0, 1] of the batched baseline curve to use
+        for efficiency comparison. The higher the quantile, the better the
+        quality of the baseline batch.
+      compared_quantile: Quantile in [0, 1] of the batched compared curve to use
+        for efficiency comparison. The higher the quantile, the better the
+        quality of the baseline batch.
+
+    Returns:
+      Sample efficiency score. This score is symmetric and always finite when
+      baseline_quantile <= compare_quantile (recommended setting).
+    """
+    rel_efficiency = self.log_efficiency_curve(compared_curve,
+                                               baseline_quantile,
+                                               compared_quantile)
+    if np.isfinite(float(rel_efficiency.ys[:, -1])):
+      # Return median relative efficiency.
+      return np.nanmedian((rel_efficiency.ys))
+    else:
+      reverse_baseline = ConvergenceCurveComparator(compared_curve)
+      reverse_efficiency = reverse_baseline.log_efficiency_curve(
+          self._baseline_curve, compared_quantile, baseline_quantile)
+      if np.isfinite(float(reverse_efficiency.ys[:, -1])):
+        return np.nanmedian((rel_efficiency.ys))
+      return -float('inf')
+
+
+def build_convergence_curve(baseline_curve: Sequence[float],
+                            compared_curve: Sequence[float]) -> List[float]:
+  """Builds a relative convergence curve (see returns for definition).
+
+  Args:
+    baseline_curve: Baseline maximization convergence curve.
+    compared_curve: Compared maximization convergence curve.
+
+  Returns:
+    A list of numbers where i-th (zero-index) element is the smallest "j" such
+    that baseline_curve[i] <= compared_curve[j]
+  """
+  convergence_curve = []
+  t1 = 0
+  for t0 in range(len(baseline_curve)):
+    while t1 < len(compared_curve) and compared_curve[t1] < baseline_curve[t0]:
+      t1 += 1
+    convergence_curve.append(float('inf') if t1 >= len(compared_curve) else t1)
+  return convergence_curve
---
+++ vizier/_src/benchmarks/analyzers/convergence_curve_test.py
@@ -0,0 +1,179 @@
+"""Tests for convergence_curve."""
+
+import numpy as np
+from vizier import pyvizier
+from vizier._src.benchmarks.analyzers import convergence_curve as convergence
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+
+def _gen_trials(values):
+  """Returns trials where trials[i] has empty metric name equal to values[i]."""
+  trials = []
+  for v in values:
+    trial = pyvizier.Trial()
+    trials.append(
+        trial.complete(
+            pyvizier.Measurement(metrics={'': pyvizier.Metric(value=v)})))
+  return trials
+
+
+class ConvergenceCurveTest(absltest.TestCase):
+
+  def test_align_xs_on_different_lengths(self):
+    c1 = convergence.ConvergenceCurve(
+        xs=np.array([1, 2, 3]),
+        ys=np.array([[2, 1, 1]]),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    c2 = convergence.ConvergenceCurve(
+        xs=np.array([1]),
+        ys=np.array([[3]]),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    aligned = convergence.ConvergenceCurve.align_xs([c1, c2])
+
+    np.testing.assert_array_equal(aligned.xs, [1, 2, 3])
+    np.testing.assert_array_equal(aligned.ys,
+                                  np.array([[2, 1, 1], [3, np.nan, np.nan]]))
+
+  def test_align_xs_on_distinct_xvalues(self):
+    c1 = convergence.ConvergenceCurve(
+        xs=np.array([1, 3, 4]),
+        ys=np.array([[2, 1, 1]]),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    c2 = convergence.ConvergenceCurve(
+        xs=np.array([2]),
+        ys=np.array([[3]]),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    aligned = convergence.ConvergenceCurve.align_xs([c1, c2])
+
+    np.testing.assert_array_equal(aligned.xs.shape, (3,))
+    np.testing.assert_array_equal(aligned.ys,
+                                  np.array([[2, 1.25, 1], [3, np.nan, np.nan]]))
+
+  def test_align_xs_on_increasing_and_dicreasing_fails(self):
+    c1 = convergence.ConvergenceCurve(
+        xs=np.array([1, 3, 4]),
+        ys=np.array([[2, 1, 1]]),
+        trend=convergence.ConvergenceCurve.YTrend.INCREASING)
+    c2 = convergence.ConvergenceCurve(
+        xs=np.array([2]),
+        ys=np.array([[3]]),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    with self.assertRaisesRegex(ValueError, 'increasing'):
+      convergence.ConvergenceCurve.align_xs([c1, c2])
+
+
+class ConvergenceCurveConverterTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      ('maximize', pyvizier.ObjectiveMetricGoal.MAXIMIZE, [[2, 2, 3]]),
+      ('minimize', pyvizier.ObjectiveMetricGoal.MINIMIZE, [[2, 1, 1]]))
+  def test_convert_basic(self, goal, expected):
+    trials = _gen_trials([2, 1, 3])
+    generator = convergence.ConvergenceCurveConverter(
+        pyvizier.MetricInformation(name='', goal=goal))
+    curve = generator.convert(trials)
+    np.testing.assert_array_equal(curve.xs, [1, 2, 3])
+    np.testing.assert_array_equal(curve.ys, expected)
+
+
+class ConvergenceComparatorTest(absltest.TestCase):
+
+  def setUp(self):
+    super(ConvergenceComparatorTest, self).setUp()
+    xs = np.array(range(0, 20))
+    xs_t = xs.reshape(1, len(xs))
+    self._baseline_curve = convergence.ConvergenceCurve(
+        xs=xs,
+        ys=np.exp(np.array([-0.9, -1.0, -1.1]).reshape(3, 1) * xs_t),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    self._baseline = convergence.ConvergenceCurveComparator(
+        self._baseline_curve)
+
+    self._worse_curves = convergence.ConvergenceCurve(
+        xs=xs,
+        ys=np.exp(-0.5 * xs_t),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    self._better_curves = convergence.ConvergenceCurve(
+        xs=xs,
+        ys=np.exp(np.array([-1.5, -1.8, -2.0]).reshape(3, 1) * xs_t),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+
+  def testGetRelativeEfficiencyCurve(self):
+    baseline_length = len(self._baseline_curve.xs)
+    rel_effiency = self._baseline.log_efficiency_curve(self._better_curves)
+    higher_quantile = self._baseline.log_efficiency_curve(
+        self._better_curves, compared_quantile=0.9)
+
+    self.assertEqual(rel_effiency.ys.shape, (1, baseline_length))
+    self.assertEqual(higher_quantile.ys.shape, (1, baseline_length))
+    # Better curves should have positive efficiency.
+    self.assertTrue((rel_effiency.ys >= 0.0).all())
+    # Higher quantile means better efficiency which means more positive scores.
+    self.assertTrue((higher_quantile.ys >= rel_effiency.ys).all())
+
+  def testGetRelativeEfficiencyFlat(self):
+    flat_curve = convergence.ConvergenceCurve(
+        xs=np.array(range(0, 20)),
+        ys=np.array([4.0, 3.0, 2.0] + [1.5] * 17).reshape(1, 20),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    comparator = convergence.ConvergenceCurveComparator(flat_curve)
+    self_eff = comparator.log_efficiency_curve(flat_curve)
+    # Relative efficiency of a curve on itself is close to 0.
+    self.assertAlmostEqual(np.linalg.norm(self_eff.ys), 0.0, delta=0.1)
+
+  def testGetRelativeEfficiencyShortCurve(self):
+    baseline_length = len(self._baseline_curve.xs)
+    short_length = round(baseline_length / 2)
+    short_curve = convergence.ConvergenceCurve(
+        xs=self._baseline_curve.xs[:short_length],
+        ys=self._baseline_curve.ys[:, :short_length],
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    short_efficiency = self._baseline.log_efficiency_curve(short_curve)
+    self.assertEqual(short_efficiency.ys.shape, (1, baseline_length))
+    self.assertEqual(float(short_efficiency.ys[:, -1]), -float('inf'))
+
+  def testGetEfficiencyScore(self):
+    # Higher compared quantile should increase score. Higher baseline
+    # quantile should decrease score.
+    median_score = self._baseline.get_log_efficiency_score(self._better_curves)
+    self.assertGreater(
+        self._baseline.get_log_efficiency_score(
+            self._better_curves, compared_quantile=0.9), median_score)
+    self.assertLess(
+        self._baseline.get_log_efficiency_score(
+            self._better_curves, baseline_quantile=0.9), median_score)
+
+  def testEffiencyScoreSymmetry(self):
+    base_score = self._baseline.get_log_efficiency_score(self._better_curves)
+    reversed_score = convergence.ConvergenceCurveComparator(
+        self._better_curves).get_log_efficiency_score(self._baseline_curve)
+    self.assertAlmostEqual(base_score, -reversed_score, delta=0.2)
+
+  def testEfficiencyScoreValue(self):
+    xs = self._baseline_curve.xs
+    xs_t = xs.reshape(1, len(xs))
+
+    worse_curves = convergence.ConvergenceCurve(
+        xs=xs,
+        ys=np.exp(-0.5 * xs_t),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    better_curves = convergence.ConvergenceCurve(
+        xs=xs,
+        ys=np.exp(np.array([-1.5, -1.8, -2.0]).reshape(3, 1) * xs_t),
+        trend=convergence.ConvergenceCurve.YTrend.DECREASING)
+    # Efficiency score for exponential curves can be approximated.
+    self.assertGreater(
+        self._baseline.get_log_efficiency_score(better_curves), 0.4)
+    self.assertLess(self._baseline.get_log_efficiency_score(worse_curves), -0.4)
+
+  def testComparatorFailure(self):
+    unknown_curve = convergence.ConvergenceCurve(
+        xs=self._baseline_curve.xs, ys=self._baseline_curve.ys)
+    with self.assertRaisesRegex(ValueError, 'increasing or decreasing'):
+      convergence.ConvergenceCurveComparator(unknown_curve)
+
+
+if __name__ == '__main__':
+  absltest.main()
