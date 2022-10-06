2796edf13688395f5a85e3e4f7dc2e15daf96428
Xingyou Song <xingyousong@google.com>
2022-10-06 09:55:48 -0700

    Adding License header to all files.
    PiperOrigin-RevId: 479339850

--- vizier/_src/algorithms/designers/bocs.py
+++ vizier/_src/algorithms/designers/bocs.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Bayesian Optimization of Combinatorial Structures (BOCS) from https://arxiv.org/abs/1806.08838.
 
 Code is a cleaned-up exact version from /BOCSpy/ in
