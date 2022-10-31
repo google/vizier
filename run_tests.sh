16bc65b6ddb4f8905e97cfee6c3a8c546d2b1ce9
Xingyou Song <xingyousong@google.com>
2022-10-31 16:32:08 -0700

    Remove references to _src/integration/ since it doesn't exist anymore
    PiperOrigin-RevId: 485177195

--- run_tests.sh
+++ run_tests.sh
@@ -14,7 +14,7 @@
 
 #!/bin/bash
 # Runs all core Python unit tests in the Vizier package.
-pytest vizier --ignore=vizier/_src/integration/ --ignore=vizier/_src/benchmarks/ --ignore=vizier/_src/algorithms/
+pytest vizier --ignore=vizier/_src/benchmarks/ --ignore=vizier/_src/algorithms/
 
 # Run algorithm tests. Disclaimer: Algorithms use multiple external libraries, some of which may break.
 pytest -n auto vizier/_src/algorithms/
