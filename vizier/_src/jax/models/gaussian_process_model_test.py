616b2b4bda469dd1664bae1331446045f60515b3
Emily Fertig <emilyaf@google.com>
2023-02-16 10:13:47 -0800

    Add a GP model and tests for input spaces that include categorical data.
    PiperOrigin-RevId: 510177666

--- vizier/_src/jax/models/gaussian_process_model_test.py
+++ vizier/_src/jax/models/gaussian_process_model_test.py
@@ -14,16 +14,17 @@
 
 from __future__ import annotations
 
-"""Tests for gaussian_process_ard."""
+"""Tests for gaussian_process_model."""
 
 from jax import random
 import numpy as np
 
 from tensorflow_probability.substrates import jax as tfp
-from vizier._src.jax import gaussian_process_ard as gp_ard
+from vizier._src.jax.models import gaussian_process_model as gp_model
 from absl.testing import absltest
 
 tfpk = tfp.math.psd_kernels
+tfpke = tfp.experimental.psd_kernels
 
 
 class GaussianProcessARDTest(absltest.TestCase):
@@ -31,13 +32,13 @@ class GaussianProcessARDTest(absltest.TestCase):
   def test_gaussian_process_ard(self):
     dim = 5
     num_obs = 10
-    coro = gp_ard.GaussianProcessARD(
+    coro = gp_model.GaussianProcessARD(
         dimension=dim,
         kernel_class=tfpk.ExponentiatedQuadratic,
         use_tfp_runtime_validation=True)
 
     obs_key, coro_key, sample_key = random.split(random.PRNGKey(0), num=3)
-    x = random.uniform(obs_key, shape=(num_obs, dim), dtype=np.float32)
+    x = random.uniform(obs_key, shape=(num_obs, dim), dtype=np.float64)
     gp, param_vals = _run_coroutine(coro(x), seed=coro_key)
     samples = gp.sample(100, seed=sample_key)
     self.assertSequenceEqual(gp.event_shape, [num_obs])
@@ -49,6 +50,48 @@ class GaussianProcessARDTest(absltest.TestCase):
     self.assertEmpty(param_vals['amplitude'].shape)
     self.assertEmpty(param_vals['observation_noise_variance'].shape)
     self.assertSequenceEqual(param_vals['inverse_length_scale'].shape, [dim])
+
+  def test_gp_model_with_categorical(self):
+    cont_dim = 5
+    cat_dim = 3
+    num_obs = 10
+    coro = gp_model.GaussianProcessARDWithCategorical(
+        dimension=tfpke.ContinuousAndCategoricalValues(cont_dim, cat_dim),
+        kernel_class=tfpk.ExponentiatedQuadratic,
+        use_tfp_runtime_validation=True,
+    )
+
+    x_cont_key, x_cat_key, coro_key, sample_key = random.split(
+        random.PRNGKey(0), num=4
+    )
+    x_cont = random.uniform(x_cont_key, shape=(num_obs, cont_dim),
+                            dtype=np.float64)
+    x_cat = random.randint(
+        x_cat_key, shape=(num_obs, cat_dim), minval=0, maxval=5
+    )
+    x = tfpke.ContinuousAndCategoricalValues(x_cont, x_cat)
+    gp, param_vals = _run_coroutine(coro(x), seed=coro_key)
+    samples = gp.sample(100, seed=sample_key)
+    self.assertSequenceEqual(gp.event_shape, [num_obs])
+    self.assertEmpty(gp.batch_shape)
+    self.assertTrue(np.isfinite(gp.log_prob(samples)).all())
+    self.assertSameElements(
+        param_vals.keys(),
+        (
+            'amplitude',
+            'inverse_length_scale_continuous',
+            'inverse_length_scale_categorical',
+            'observation_noise_variance',
+        ),
+    )
+    self.assertEmpty(param_vals['amplitude'].shape)
+    self.assertEmpty(param_vals['observation_noise_variance'].shape)
+    self.assertSequenceEqual(
+        param_vals['inverse_length_scale_continuous'].shape, [cont_dim]
+    )
+    self.assertSequenceEqual(
+        param_vals['inverse_length_scale_categorical'].shape, [cat_dim]
+    )
 
 
 def _run_coroutine(g, seed):
