34c229908367a9f719461d0eac3a8a1dc7781a94
Chansoo Lee <chansoo@google.com>
2023-05-25 09:01:24 -0700

    Temporarily disable algorithms and raytune test until the next jaxopt release
    PiperOrigin-RevId: 535275239

--- run_tests.sh
+++ run_tests.sh
@@ -16,9 +16,11 @@
 # Runs all core Python unit tests in the Vizier package.
 pytest vizier --ignore=vizier/_src/benchmarks/ --ignore=vizier/_src/algorithms/
 
-# Run algorithm tests. Disclaimer: Algorithms use multiple external libraries, some of which may break.
+# Run algorithm tests. Disclaimer: These tests require multiple external libraries, some of which may break.
 pytest -n auto vizier/_src/algorithms/
 
-# Run benchmark tests. Disclaimer: Benchmarks use multiple external libraries, some of which may break.
+# Run benchmark tests. Disclaimer: These tests require multiple external libraries, some of which may break.
 pytest -n auto vizier/_src/benchmarks/
 
+# Run pyglove tests. Disclaimer: These tests require multiple external libraries, some of which may break.
+pytest -n auto vizier/_src/pyglove/
