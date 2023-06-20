40d3eff749fbae45020f9e40d95945e577623914
Richard Zhang <qiuyiz@google.com>
2023-06-20 13:11:09 -0700

    Test that VizierSearcher can use StudyConfig.
    PiperOrigin-RevId: 542026552

--- vizier/_src/raytune/vizier_search_test.py
+++ vizier/_src/raytune/vizier_search_test.py
@@ -16,13 +16,12 @@ from __future__ import annotations
 
 """Test for VizierSearch. Cannot be tested internally but can be on GitHub."""
 from ray import tune
+from vizier._src.raytune import converters
 from vizier._src.raytune import run_tune
 from vizier._src.raytune import vizier_search
+from vizier.benchmarks import experimenters
 from vizier.service import clients
-
-# from vizier._src.raytune import converters
-# from vizier.benchmarks import experimenters
-# from vizier.service import pyvizier as vz
+from vizier.service import pyvizier as vz
 
 from absl.testing import absltest
 
@@ -33,24 +32,25 @@ clients.environment_variables.servicer_use_sql_ram()
 
 class VizierSearchTest(absltest.TestCase):
 
-  # def test_search_with_study_config(self):
-  # dim = 4
-  # bbob_factory = experimenters.BBOBExperimenterFactory(name='Sphere', dim=dim)
-  # exptr = bbob_factory()
+  def test_search_with_study_config(self):
+    dim = 4
+    bbob_factory = experimenters.BBOBExperimenterFactory(name='Sphere', dim=dim)
+    exptr = bbob_factory()
 
-  # study_config = vz.StudyConfig.from_problem(exptr.problem_statement())
-  # study_config.algorithm = 'RANDOM_SEARCH'
-  # self.assertLen(study_config.search_space.parameters, dim)
-  # searcher = vizier_search.VizierSearch('test study', study_config)
+    study_config = vz.StudyConfig.from_problem(exptr.problem_statement())
+    self.assertLen(study_config.search_space.parameters, dim)
+    searcher = vizier_search.VizierSearch(
+        'test study', study_config, algorithm='RANDOM_SEARCH'
+    )
 
-  # trainable = converters.ExperimenterConverter.to_callable(exptr)
-  # tuner = tune.Tuner(
-  #     trainable,
-  #     param_space=None,
-  #     tune_config=tune.TuneConfig(num_samples=10, search_alg=searcher),
-  # )
-  # tuner.fit()
-  # self.assertLen(tuner.get_results(), 10)
+    trainable = converters.ExperimenterConverter.to_callable(exptr)
+    tuner = tune.Tuner(
+        trainable,
+        param_space=None,
+        tune_config=tune.TuneConfig(num_samples=5, search_alg=searcher),
+    )
+    tuner.fit()
+    self.assertLen(tuner.get_results(), 5)
 
   # def test_search_with_ray_search_space(self):
   #  dim = 4
