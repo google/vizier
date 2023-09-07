5aad0c02624006b7ef6c2d16615ce442a151a2bc
Daiyi Peng <daiyip@google.com>
2023-09-07 10:06:04 -0700

    Improving failover handling in VizierBackend.
    PiperOrigin-RevId: 563466499

--- vizier/_src/pyglove/backend.py
+++ vizier/_src/pyglove/backend.py
@@ -16,9 +16,11 @@ from __future__ import annotations
 
 """Cross-Platform Backend."""
 import enum
+import functools
 import getpass
+import random
+import threading
 import time
-
 from typing import Any, Dict, Optional, Sequence, Type, Union
 
 from absl import logging
@@ -73,12 +75,47 @@ class VizierBackend(pg.tuning.Backend):
   tuner_cls: Type[client.VizierTuner] = attrs.field()
 
   # Instance-level variables.
+
+  #
+  # Flags passed from `pg.sample`:
+  #
+
+  # Worker group - workers that belong to the same group will share the same
+  # Vizier client ID.
+  _group: Union[None, int, str] = attrs.field()
+
+  # Max number of examples to sample.
+  _num_examples: Optional[int] = attrs.field()
+
+  # Prior study IDs for transfer learning.
+  _prior_study_ids: Optional[Sequence[str]] = attrs.field()
+
+  # If True, add the completed trials from prior studies. Otherwise, simply
+  # warm up the algorithm using these trials without adding them to the current
+  # study.
+  _add_prior_trials: bool = attrs.field()
+
+  #
+  # Internal states.
+  #
+
   _tuner: client.VizierTuner
-  _name: str = attrs.field()
-  _study: client_abc.StudyInterface = attrs.field()
-  _converter: converters.VizierConverter = attrs.field()
+  _dna_spec: pg.DNASpec
   _algorithm: pg.geno.DNAGenerator = attrs.field()
+  _early_stopping_policy: Optional[pg.tuning.EarlyStoppingPolicy] = (
+      attrs.field()
+  )
+
+  _study_owner: str = attrs.field()
+  _study_name: ExpandedStudyName = attrs.field()
+  _converter: converters.VizierConverter = attrs.field()
+
+  _study: client_abc.StudyInterface = attrs.field()
   _suggestion_generator: Any = attrs.field()
+
+  _run_mode: TunerMode = attrs.field()
+  _auto_election_thread: Optional[threading.Thread] = attrs.field()
+  _is_active: bool = attrs.field()
 
   def __init__(
       self,
@@ -95,11 +132,25 @@ class VizierBackend(pg.tuning.Backend):
       is_chief: Optional[bool] = None,
   ):
     self._tuner = self.tuner_cls()
-    self._algorithm = algorithm
-    study_owner = study_owner or self.default_owner
-    prior_study_ids = prior_study_ids or tuple()
-    name = self._expand_name(name)
 
+    self._dna_spec = dna_spec
+    self._algorithm = algorithm
+    self._early_stopping_policy = early_stopping_policy
+
+    self._group = group
+    self._num_examples = num_examples
+    self._prior_study_ids = prior_study_ids or tuple()
+    self._add_prior_trials = add_prior_trials
+
+    self._study_owner = study_owner or self.default_owner
+    self._study_name = self._expand_name(name)
+
+    # Set up converter based on the search space and metrics to optimize.
+    self._converter = converters.VizierConverter.from_dna_spec(
+        dna_spec, metrics_to_optimize
+    )
+
+    # Detecting mode based on is_chef flag.
     if is_chief:
       mode = TunerMode.PRIMARY
     elif is_chief is None:
@@ -107,118 +158,196 @@ class VizierBackend(pg.tuning.Backend):
     else:
       mode = TunerMode.SECONDARY
 
-    self._converter = converters.VizierConverter.from_dna_spec(
-        dna_spec, metrics_to_optimize
-    )
+    self._run_mode = mode
 
-    # Load or create study.
+    # Setup the Vizier study and the suggestion generator.
+    is_chief = self._setup_study()
+    self._suggestion_generator = self._create_suggestion_generator()
+
+    # Start Pythia if needed.
+    self._auto_election_thread = None
+    self._is_active = True
+
+    if self._need_pythia_service:
+      pg.logging.info(
+          "Study '%s/%s' will be served by Pythia service hosted on %r. "
+          'Algorithm=%r, EarlyStoppingPolicy=%r.',
+          self._study_owner,
+          self._study_name,
+          self._get_chief_tuner_id(),
+          self._algorithm,
+          self._early_stopping_policy,
+      )
+
+      if is_chief:
+        pg.logging.info(
+            "Starting hosting Pythia service for study '%s/%s' as PRIMARY. ",
+            self._study_owner,
+            self._study_name,
+        )
+        self._start_pythia()
+      if self._run_mode == TunerMode.AUTO:
+        self._auto_election_thread = threading.Thread(
+            target=self._auto_elect_primary_if_needed
+        )
+        self._auto_election_thread.start()
+    else:
+      pg.logging.info(
+          'Pythia service is not required as both the search algorithm and '
+          'early stopping policy are either Vizier built-in or served from '
+          'remote Pythia endpoint. Algorithm=%r, EarlyStoppingPolicy=%r.',
+          self._algorithm,
+          self._early_stopping_policy,
+      )
+
+  @property
+  def _host_pythia_algorithm(self) -> bool:
+    """Returns True if the algorithm is hosted on Pythia."""
+    return not isinstance(self._algorithm, algorithms.PseudoAlgorithm)
+
+  @property
+  def _need_pythia_service(self) -> bool:
+    """Returns True if pythia service is needed."""
+    return self._host_pythia_algorithm
+
+  def _setup_study(self) -> bool:
+    """Sets up Vizier study, returns True if current worker is the chief."""
+    study_descriptor = f'{self._study_owner}/{self._study_name}'
+
     try:
-      self._study = self._tuner.load_study(study_owner, name)
-      # Study exists.
-      if mode == TunerMode.AUTO:
-        if self._tuner.ping_tuner(self._get_chief_tuner_id()):
-          mode = TunerMode.SECONDARY
-        else:
-          # NOTE(daiyip): there could be a race condition that multiple workers
-          # elect themselves as the new primary, and all of them regard
-          # themselves as the elected. When this happens, multiple workers
-          # may be hosting the Pythia service for this study. However, the study
-          # will use one address (BNS of the latest elected worker) as the
-          # Pythia endpoint. Besides, all states are stored in the study,
-          # restart of workers will pick up these states from the study.
-          # This is done by replaying existing trials to recompute the states of
-          # the search algorithm. Therefore, it does not matter which worker is
-          # chosen to serve the study, which should work equally well.
-          # On the other hand, it is cheap to serve a PythiaService without
-          # incoming queries. Therefore, we do not handle this race conditions
-          # with expensive distributed locks.
-          mode = TunerMode.PRIMARY
-          self._register_self_as_primary()
+      self._study = self._tuner.load_study(self._study_owner, self._study_name)
+      pg.logging.info(
+          'Connecting tuner to existing study %r... ', study_descriptor
+      )
+      chief_tuner_id = self._get_chief_tuner_id()
+
+      if self._run_mode != TunerMode.SECONDARY and not self._tuner.ping_tuner(
+          chief_tuner_id
+      ):
+        # NOTE(daiyip): there could be a race condition that multiple workers
+        # elect themselves as the new primary, and all of them regard
+        # themselves as the elected. When this happens, multiple workers
+        # may be hosting the Pythia service for this study. However, the study
+        # will use one address (BNS of the latest elected worker) as the
+        # Pythia endpoint. Besides, all states are stored in the study,
+        # restart of workers will pick up these states from the study.
+        # This is done by replaying existing trials to recompute the states of
+        # the search algorithm. Therefore, it does not matter which worker is
+        # chosen to serve the study, which should work equally well.
+        # On the other hand, it is cheap to serve a PythiaService without
+        # incoming queries. Therefore, we do not handle this race conditions
+        # with expensive distributed locks.
+        chief_tuner_id = self._register_self_as_primary()
+
+      is_chief = chief_tuner_id == self._tuner_id
+      if self._run_mode == TunerMode.SECONDARY and is_chief:
+        raise ValueError(
+            f'Inconsistent primary tuner: {self._tuner_id!r} is running as '
+            f'secondary but study {study_descriptor!r} indicates otherwise.'
+        )
+      elif self._run_mode == TunerMode.PRIMARY and not is_chief:
+        raise ValueError(
+            f'Inconsistent tuner mode: {self._tuner_id!r} is running in '
+            f'PRIMARY mode but study {study_descriptor!r} already has a '
+            f'different primary {chief_tuner_id!r}.\n'
+            'Please check if you are launching different experiments using '
+            'the same Vizier study.'
+        )
+
+      pg.logging.info(
+          'Tuner is running in %s mode with existing study %r.',
+          'PRIMARY' if is_chief else 'SECONDARY',
+          study_descriptor,
+      )
+
+      return is_chief
     except KeyError:
       # Study does not exist.
-      if mode == TunerMode.SECONDARY:
-        self._study = self._wait_for_study(study_owner, name)
+      if self._run_mode == TunerMode.SECONDARY:
+        pg.logging.info(
+            'Start tuner as secondary. Waiting for study %r to be created...',
+            study_descriptor,
+        )
+        self._study = self._wait_for_study(self._study_owner, self._study_name)
       else:
-        mode = TunerMode.PRIMARY  # We will make this a chief
+        mode_str = 'PRIMARY' if self._run_mode == TunerMode.PRIMARY else 'AUTO'
+        pg.logging.info(
+            'Start tuner in %s mode. Attempting to create study %r...',
+            mode_str,
+            study_descriptor,
+        )
+
+        # Create a new study.
         problem = self._converter.problem_or_dummy
-        local_tuner_id = self._tuner.get_tuner_id(self._algorithm)
         problem.metadata.ns(constants.METADATA_NAMESPACE)[
             constants.STUDY_METADATA_KEY_TUNER_ID
-        ] = local_tuner_id
+        ] = self._tuner_id
         self._study = self._tuner.create_study(
             problem,
             self._converter,
-            study_owner,
-            name,
+            self._study_owner,
+            self._study_name,
             self._algorithm,
-            early_stopping_policy,
+            self._early_stopping_policy,
         )
-        if local_tuner_id == self._get_chief_tuner_id():
-          # For multi-thread scenario, `local_tuner_id` will be the same for
-          # all the worker threads, therefore there is a chance that multiple
-          # worker threads consider themselves as PRIMARY. This does not matter
-          # since there is only one Pythia service shared across them.
-          mode = TunerMode.PRIMARY  # We will make this a chief
-          if add_prior_trials:
-            # Trials are added to the study directly upon creation.
-            trials: Sequence[vz.Trial] = self._load_prior_trials(
-                prior_study_ids
-            )
-            for trial in trials:
-              self._study._add_trial(trial)
-        else:
-          mode = TunerMode.SECONDARY
 
-    # Set up the generator.
-    def _suggestion_generator():
-      while (
-          num_examples is None or len(list(self._study.trials())) < num_examples
-      ):
-        trials = self._study.suggest(
-            count=1, client_id=self._tuner.get_group_id(group)
-        )
-        if not trials:
-          return
-        for trial in trials:
-          yield core.Feedback(trial, self._converter)
+      # Perform post-study actions.
+      is_chief = self._tuner_id == self._get_chief_tuner_id()
+      if is_chief:
+        self._on_study_created()
 
-    self._suggestion_generator = _suggestion_generator()
+      pg.logging.info(
+          'Tuner is running in %s mode with newly created study %r.',
+          'PRIMARY' if is_chief else 'SECONDARY',
+          study_descriptor,
+      )
+      return is_chief
 
-    if mode != TunerMode.PRIMARY or (
-        isinstance(self._algorithm, algorithms.PseudoAlgorithm)
+  def _on_study_created(self):
+    # For multi-thread scenario, `local_tuner_id` will be the same for
+    # all the worker threads, therefore there is a chance that multiple
+    # worker threads consider themselves as PRIMARY. This does not matter
+    # since there is only one Pythia service shared across them.
+    if self._add_prior_trials:
+      # Trials are added to the study directly upon creation.
+      trials: Sequence[vz.Trial] = self._load_prior_trials()
+      for trial in trials:
+        self._study._add_trial(trial)  # pylint: disable=protected-access
+
+  def _create_suggestion_generator(self):
+    """Creates a suggestion generator."""
+    while (
+        self._num_examples is None
+        or len(list(self._study.trials())) < self._num_examples
     ):
-      # nothing more to do
-      return
+      trials = self._study.suggest(
+          count=1, client_id=self._tuner.get_group_id(self._group)
+      )
+      if not trials:
+        return
+      for trial in trials:
+        yield core.Feedback(trial, self._converter)
 
+  def _start_pythia(self) -> None:
+    """Starts Pythia service."""
     # Start pythia service.
     self._tuner.start_pythia_service(_global_policy_cache)
 
     # Set up the policy.
-    self._algorithm.setup(dna_spec)
+    self._algorithm.setup(self._dna_spec)
     prior_trials = tuple()
-    if not add_prior_trials:
+    if not self._add_prior_trials:
       # Trials are added to the algorithm only.
-      prior_trials: Sequence[vz.Trial] = self._load_prior_trials(
-          prior_study_ids
-      )
-    policy = self._create_policy(early_stopping_policy, prior_trials)
+      prior_trials: Sequence[vz.Trial] = self._load_prior_trials()
+    policy = self._create_policy(prior_trials)
     # Connect the service with policy.
-    self._register(study_owner, name, policy)
+    self._register(self._study_owner, self._study_name, policy)
 
   def _create_policy(
       self,
-      early_stopping_policy: Optional[pg.tuning.EarlyStoppingPolicy],
       prior_trials: Sequence[vz.Trial],
   ) -> TunerPolicy:
-    """Creates a pythia policy.
-
-    Args:
-      early_stopping_policy:
-      prior_trials:
-
-    Returns:
-      Policy.
-    """
+    """Creates a pythia policy."""
     if prior_trials:
 
       def get_trial_history(vizier_trials):
@@ -235,7 +364,7 @@ class VizierBackend(pg.tuning.Backend):
         self._tuner.pythia_supporter(self._study),
         self._converter,
         self._algorithm,
-        early_stopping_policy=early_stopping_policy,
+        early_stopping_policy=self._early_stopping_policy,
     )
 
   def _get_chief_tuner_id(self) -> str:
@@ -249,20 +378,60 @@ class VizierBackend(pg.tuning.Backend):
           f'Metadata does not exist in study: {self._study.resource_name}'
       ) from e
 
+  @functools.lru_cache(maxsize=None)d_property
+  def _tuner_id(self) -> str:
+    """Returns the tuner id of current ."""
+    return self._tuner.get_tuner_id(self._algorithm)
+
   def _register_self_as_primary(self) -> str:
     metadata = vz.Metadata()
-    tuner_id = self._tuner.get_tuner_id(self._algorithm)
     metadata.ns(constants.METADATA_NAMESPACE)[
         constants.STUDY_METADATA_KEY_TUNER_ID
-    ] = self._tuner.get_tuner_id(self._algorithm)
+    ] = self._tuner_id
     self._study.update_metadata(metadata)
     self._tuner.use_pythia_for_study(self._study)
-    return tuner_id
+    return self._tuner_id
+
+  def _auto_elect_primary_if_needed(self) -> None:
+    """Automatically elect primary tuner if previous primary is offline."""
+    assert self._need_pythia_service
+    assert self._run_mode == TunerMode.AUTO
+
+    while self._is_active:
+      chief_tuner_id = self._get_chief_tuner_id()
+      if not self._tuner.ping_tuner(chief_tuner_id):
+        # NOTE(daiyip): there could be a race condition that multiple workers
+        # elect themselves as the new primary, and all of them regard
+        # themselves as the elected. When this happens, multiple workers
+        # may be hosting the Pythia service for this study. However, the study
+        # will use one address (BNS of the latest elected worker) as the
+        # Pythia endpoint. Besides, all states are stored in the study,
+        # restart of workers will pick up these states from the study.
+        # This is done by replaying existing trials to recompute the states of
+        # the search algorithm. Therefore, it does not matter which worker is
+        # chosen to serve the study, which should work equally well.
+        # On the other hand, it is cheap to serve a PythiaService without
+        # incoming queries. Therefore, we do not handle this race conditions
+        # with expensive distributed locks.
+        new_chief_tuner_id = self._register_self_as_primary()
+        self._is_primary = new_chief_tuner_id == self._tuner_id
+        if self._is_primary:
+          pg.logging.warning(
+              'Primary tuner has been switched from %s to %s.',
+              chief_tuner_id,
+              new_chief_tuner_id,
+          )
+          self._start_pythia()
+      time.sleep(random.randint(50, 70))
 
   def next(self) -> pg.tuning.Feedback:
     """Gets the next tuning feedback object."""
-    trial = next(self._suggestion_generator)  # pytype: disable=wrong-arg-types
-    return core.Feedback(self._study.get_trial(trial.id), self._converter)
+    try:
+      trial = next(self._suggestion_generator)  # pytype: disable=wrong-arg-types
+      return core.Feedback(self._study.get_trial(trial.id), self._converter)
+    except StopIteration as e:
+      self._is_active = False
+      raise e
 
   def _wait_for_study(
       self, owner: str, name: ExpandedStudyName
@@ -284,11 +453,9 @@ class VizierBackend(pg.tuning.Backend):
         )
         time.sleep(60)
 
-  def _load_prior_trials(
-      self, prior_study_ids: Optional[Sequence[str]]
-  ) -> list[vz.Trial]:
+  def _load_prior_trials(self) -> list[vz.Trial]:
     trials = []
-    for prior in prior_study_ids:
+    for prior in self._prior_study_ids:
       trials.extend(
           self._tuner.load_prior_study(prior)
           .trials(vz.TrialFilter(status=vz.TrialStatus.COMPLETED))
