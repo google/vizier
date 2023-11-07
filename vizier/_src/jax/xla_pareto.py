c7ac5d266e2aad563c32c1ddf80dcc437838a412
Chansoo Lee <chansoo@google.com>
2023-11-06 17:29:07 -0800

    Add the jitted pareto rank function to the public `xla_pareto` module. Update jaxtyping.
    PiperOrigin-RevId: 580007645

--- vizier/_src/jax/xla_pareto.py
+++ vizier/_src/jax/xla_pareto.py
@@ -16,26 +16,20 @@ from __future__ import annotations
 
 """Fast pareto frontier computation using Jax."""
 import functools
-from typing import Any
 
 import jax
 from jax import numpy as jnp
+import jaxtyping as jt
 import numpy as np
-
 from vizier._src.pyvizier.multimetric import pareto_optimal
 
-# Type aliases for readability only.
-# We don't over-specify for compatibility between jax and np.
-Matrix = Any
-Vector = Any
-Scalar = Any
 
-
-# y2 dominates y1 if y2 >= y1 coordinate wise. This is strict if we also have
-# y2 > y1 in some coordinate.
-def _jax_dominated(y1: Vector,
-                   y2: Vector,
-                   strict: bool = True) -> Scalar:  # ([a], [a]) -> []
+def _is_dominated(
+    y1: jt.Float[jt.Array, "M"],
+    y2: jt.Float[jt.Array, "M"],
+    strict: bool = True,
+) -> jt.Bool[jt.Array, ""]:
+  """True if y2 > y1 (or y2 >= y1 if strict is False) every coordinate."""
   dominated_or_equal = jnp.all(y1 <= y2)
   if strict:
     return dominated_or_equal & jnp.any(y2 > y1)
@@ -43,48 +37,51 @@ def _jax_dominated(y1: Vector,
     return dominated_or_equal
 
 
-def _is_pareto_optimal_against(yy: Matrix, baseline: Matrix, *,
-                               strict: bool) -> Vector:
+@functools.partial(jax.jit, static_argnames="strict")
+def _is_pareto_optimal_against(
+    yy: jt.Float[jt.Array, "B1 M"],
+    baseline: jt.Float[jt.Array, "B2 M"],
+    *,
+    strict: bool,
+) -> jt.Bool[jt.Array, "B1"]:
   """Computes if nothing in `baseline` dominates `yy`.
 
   Args:
-    yy: array of shape [N_y, M] where M is number of metrics.
-    baseline: array of shape [N_b, M] where M is number of metrics.
+    yy: array of shape [B1, M] where M is number of metrics.
+    baseline: array of shape [B2, M] where M is number of metrics.
     strict: If true, strict dominance is used.
 
   Returns:
-    Boolean array of shape [N_y]
+    Boolean array of shape [B1]
   """
   jax_dominated_mv = jax.vmap(
-      functools.partial(_jax_dominated, strict=strict), (None, 0),
-      0)  #  ([b,a], [a]) -> [b]
+      functools.partial(_is_dominated, strict=strict), (None, 0), 0
+  )  #  ([b,a], [a]) -> [b]
   jax_dominated_mm = jax.vmap(jax_dominated_mv, (0, None),
                               0)  #  ([b,a], [c,a]) -> [b,c]
   return jnp.logical_not(jnp.any(jax_dominated_mm(yy, baseline),
                                  axis=-1))  # N_y, N_b -> N_y
 
 
-_jax_is_pareto_optimal_against = jax.jit(
-    _is_pareto_optimal_against, static_argnames='strict')
-
-
-def is_frontier(ys: Matrix,
-                *,
-                num_shards: int = 10,
-                verbose: bool = False) -> jnp.ndarray:
-  """Efficiently compute `_jax_is_pareto_optimal_against(ys, ys, strict=True)`.
+def is_frontier(
+    ys: jt.Float[jt.ArrayLike, "B M"],
+    *,
+    num_shards: int = 10,
+    verbose: bool = False,
+) -> jt.Bool[jt.ArrayLike, "B"]:
+  """Efficiently compute `_is_pareto_optimal_against(ys, ys, strict=True)`.
 
   Divide `ys` into shards and gradually trim down the candidates.
 
   Args:
-    ys: Array of shape [N_y, M] where M is number of metrics.
+    ys: Array of shape [B, M] where M is number of metrics.
     num_shards: Each sharding results in filtering, i.e. indexing the array with
       boolean vector. This operation can be very expensive and dominate the cost
-      of computation. Use a moderate number sublinear in N_y, e.g. log(N).
+      of computation. Use a moderate number sublinear in B, e.g. log(B).
     verbose:
 
   Returns:
-    Boolean Array of shape [N_y].
+    Boolean numpy Array of shape [B].
   """
   idx = np.linspace(0, ys.shape[0], num_shards).astype(np.int32)
   idx = list(reversed(idx))
@@ -95,8 +92,8 @@ def is_frontier(ys: Matrix,
     candidates = ys[frontier]
     # Filter candidates by comparing against the slice.
     if verbose:
-      print(f'Compare {len(candidates)} against {begin}:{end}.')
-    tt = _jax_is_pareto_optimal_against(candidates, ys[begin:end], strict=True)
+      print(f"Compare {len(candidates)} against {begin}:{end}.")
+    tt = _is_pareto_optimal_against(candidates, ys[begin:end], strict=True)
     frontier[frontier] = tt
   return frontier
 
@@ -113,29 +110,32 @@ class JaxParetoOptimalAlgorithm(pareto_optimal.BaseParetoOptimalAlgorithm):
                                 strict: bool) -> np.ndarray:
     """Jax-enabled optimality/domination algorithm. See base class."""
     return np.array(
-        _jax_is_pareto_optimal_against(points, dominating_points, strict),
-        dtype=bool)
+        _is_pareto_optimal_against(points, dominating_points, strict=strict),
+        dtype=bool,
+    )
 
 
-def get_frontier(ys: Matrix,
-                 *,
-                 num_shards: int = 10,
-                 verbose: bool = True) -> jnp.ndarray:
-  """Efficiently compute `ys[_jax_is_pareto_optimal_against(ys, ys, strict=True)]` using iterative filtering.
+def get_frontier(
+    ys: jt.Float[jt.ArrayLike, "B M"],
+    *,
+    num_shards: int = 10,
+    verbose: bool = True,
+) -> jnp.ndarray:
+  """Efficiently compute `ys[_is_pareto_optimal_against(ys, ys, strict=True)]` using iterative filtering.
 
   Divide `ys` into shards and gradually trim down the candidates.
   `get_frontier` doesn't call `is_frontier`, because `get_frontier` runs faster
   by not slicing the full `ys` every iteration.
 
   Args:
-    ys: Array of shape [N_y, M] where M is number of metrics.
+    ys: Array of shape [B, M] where M is number of metrics.
     num_shards: Each sharding results in filtering, i.e. indexing the array with
       boolean vector. This operation can be very expensive and dominate the cost
-      of computation. Use a moderate number sublinear in N_y, e.g. log(N).
+      of computation. Use a moderate number sublinear in B, e.g. log(B).
     verbose:
 
   Returns:
-    Array of shape [N_y, M].
+    Array of shape [B, M].
   """
   idx = np.linspace(0, ys.shape[0], num_shards).astype(np.int32)
   idx = list(reversed(idx))
@@ -145,24 +145,28 @@ def get_frontier(ys: Matrix,
     # Filter candidates by comparing against the slice.
     if verbose:
       # Use print. This method won't run in production anyways.
-      print(f'Compare {len(candidates)} against {begin}:{end}.')
-    tt = _jax_is_pareto_optimal_against(candidates, ys[begin:end], strict=True)
+      print(f"Compare {len(candidates)} against {begin}:{end}.")
+    tt = _is_pareto_optimal_against(candidates, ys[begin:end], strict=True)
     candidates = candidates[tt]
   return candidates
 
 
 @jax.jit
-def pareto_rank(ys: Matrix) -> jnp.ndarray:
+def pareto_rank(ys: jt.Float[jt.ArrayLike, "B M"]) -> jt.Int[jt.ArrayLike, "B"]:
+  """Returns the pareto rank."""
   jax_dominated_mv = jax.vmap(
-      functools.partial(_jax_dominated, strict=True), (None, 0),
-      0)  #  ([b,a], [a]) -> [b]
-  jax_dominated_mm = jax.vmap(jax_dominated_mv, (0, None),
-                              0)  #  ([b,a], [c,a]) -> [b,c]
+      functools.partial(_is_dominated, strict=True), (None, 0), 0
+  )  #  ([b,a], [a]) -> [b]
+  jax_dominated_mm = jax.vmap(
+      jax_dominated_mv, (0, None), 0
+  )  #  ([b,a], [c,a]) -> [b,c]
   domination_matrix = jax_dominated_mm(ys, ys)
   return jnp.sum(domination_matrix, axis=1)
 
 
-def _cum_hypervolume_origin(points: Matrix, vector: Vector) -> Vector:
+def _cum_hypervolume_origin(
+    points: jt.Float[jt.ArrayLike, "B M"], vector: jt.Float[jt.Array, "... M"]
+) -> jt.Float[jt.Array, "B"]:
   """Returns a randomized approximation of the cumulative dominated hypervolume.
 
   See Section 3, Lemma 5 of https://arxiv.org/pdf/2006.04655.pdf for a fuller
@@ -184,10 +188,10 @@ def _cum_hypervolume_origin(points: Matrix, vector: Vector) -> Vector:
   return jax.lax.cummax(coordinate_min_ratio, axis=0)**len(vector)
 
 
-_cum_hypervolume_mm = jax.vmap(_cum_hypervolume_origin, (None, 0),
-                               0)  #  ([b,a], [c,a]) -> [b,c]
-
-
 @jax.jit
-def jax_cum_hypervolume_origin(points, vectors):
-  return jnp.mean(_cum_hypervolume_mm(points, vectors), axis=0)
+def jax_cum_hypervolume_origin(
+    points: jt.Float[jt.ArrayLike, "B M"], vectors: jt.Float[jt.Array, "B2 M"]
+) -> jt.Float[jt.Array, "B B2"]:
+  #  ([B,M], [B2,M]) -> [B,B2]
+  cum_hypervolume_mm = jax.vmap(_cum_hypervolume_origin, (None, 0), 0)
+  return jnp.mean(cum_hypervolume_mm(points, vectors), axis=0)
--- vizier/pyvizier/multimetric/xla_pareto.py
+++ vizier/pyvizier/multimetric/xla_pareto.py
@@ -16,6 +16,7 @@ from __future__ import annotations
 
 """XLA-enabled algorithm for determining pareto optimality."""
 # pylint: disable=unused-import
-
+from vizier._src.jax.xla_pareto import is_frontier
 from vizier._src.jax.xla_pareto import jax_cum_hypervolume_origin
 from vizier._src.jax.xla_pareto import JaxParetoOptimalAlgorithm
+from vizier._src.jax.xla_pareto import pareto_rank
