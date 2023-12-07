097adc744632e06c5e93bb7f4ebc8d0cf7b927c8
Lior Belenki <belenkil@google.com>
2023-12-07 13:50:15 -0800

    Add failing designer for testing.
    PiperOrigin-RevId: 588898376

---
+++ vizier/_src/algorithms/testing/failing.py
@@ -0,0 +1,72 @@
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+"""Failing designers used for testing."""
+
+from typing import Optional, Sequence
+from vizier import algorithms as vza
+from vizier import pyvizier as vz
+from vizier._src.algorithms.designers import random
+
+
+class FailedSuggestError(Exception):
+  """Exception to raise during failing suggest call."""
+
+
+class FailingDesigner(vza.Designer):
+  """Failing Designer.
+
+  The designer raises exception at every suggest call.
+  """
+
+  def update(
+      self, completed: vza.CompletedTrials, all_active: vza.ActiveTrials
+  ) -> None:
+    pass
+
+  def suggest(
+      self, count: Optional[int] = None
+  ) -> Sequence[vz.TrialSuggestion]:
+    raise FailedSuggestError()
+
+
+class AlternateFailingDesigner(vza.Designer):
+  """Alternate Failing Designer.
+
+  The designer raises exception at every second suggest call.
+
+  Note: the designer doesn't persist a state across different instantiations and
+  therefore should only be used during in memory run.
+  """
+
+  def __init__(self, search_space: vz.SearchSpace):
+    self._suggest_count = 0
+    self._random_designer = random.RandomDesigner(search_space)
+
+  def update(
+      self, completed: vza.CompletedTrials, all_active: vza.ActiveTrials
+  ) -> None:
+    pass
+
+  def suggest(
+      self, count: Optional[int] = None
+  ) -> Sequence[vz.TrialSuggestion]:
+    count = count or 1
+    self._suggest_count += count
+    if self._suggest_count % 2 == 0 or count > 1:
+      raise FailedSuggestError()
+    else:
+      return self._random_designer.suggest(1)
---
+++ vizier/_src/algorithms/testing/failing_test.py
@@ -0,0 +1,42 @@
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+"""Tests for failing."""
+
+from vizier import pyvizier as vz
+from vizier._src.algorithms.testing import failing
+from absl.testing import absltest
+
+
+class FailingTest(absltest.TestCase):
+
+  def test_failing_designer(self):
+    failing_designer = failing.FailingDesigner()
+    with self.assertRaises(failing.FailedSuggestError):
+      failing_designer.suggest(1)
+
+  def test_alternate_failing_designer(self):
+    search_space = vz.SearchSpace()
+    search_space.root.add_float_param("x", 0.0, 1.0)
+    alt_failing_designer = failing.AlternateFailingDesigner(search_space)
+    for _ in range(5):
+      alt_failing_designer.suggest(1)
+      with self.assertRaises(failing.FailedSuggestError):
+        alt_failing_designer.suggest(1)
+
+
+if __name__ == "__main__":
+  absltest.main()
