8d9643f72fe382ecebbc22610e932d1ba704ae46
Sagi Perel <sagipe@google.com>
2025-10-09 15:06:36 -0700

    Fix test breakage in proto comparison.
    PiperOrigin-RevId: 817353612

--- vizier/_src/pyvizier/oss/study_config_test.py
+++ vizier/_src/pyvizier/oss/study_config_test.py
@@ -165,13 +165,7 @@ class StudyConfigTest(parameterized.TestCase):
     self.assertFalse(sc.is_single_objective)
 
     round_trip_proto = sc.to_proto()
-    compare.assertProto2Equal(
-        self,
-        study_config_proto,
-        round_trip_proto,
-        ignore_repeated_fields_order=True,
-        dedup_repeated_fields=True,
-    )
+    compare.assertProto2SameElements(self, study_config_proto, round_trip_proto)
 
     _ = vz.StudyConfig.from_problem(sc.to_problem())  # smoke test.
 
